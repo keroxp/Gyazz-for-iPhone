9837186b665d6bc14b6f651e93c067ae08b7fe79
Yusuke Sakurai <kerokerokerop@gmail.com>
2013-12-28 09:39:29 +0900

    テストターゲットを追加

---
+++ GyazzForiPhoneTests/GyazzForiPhoneTests.m
@@ -0,0 +1,34 @@
+//
+//  GyazzForiPhoneTests.m
+//  GyazzForiPhoneTests
+//
+//  Created by 桜井雄介 on 2013/12/28.
+//  Copyright (c) 2013年 Masui Lab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GyazzForiPhoneTests : XCTestCase
+
+@end
+
+@implementation GyazzForiPhoneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

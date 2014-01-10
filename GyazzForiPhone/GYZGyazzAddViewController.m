7e2f172504a58c6a405a74e32c131de6dfdb8825
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-01-10 18:54:40 +0900

    Revert "GYAZ追加時に認証が通らないバグを修正"
    - 存在しないgyazzでも登録できてしまうことが判明したのでrevert
    
    This reverts commit f442c877ad923f3a8c8f98812552b7ddb7dcd8f3.

--- GyazzForiPhone/GYZGyazzAddViewController.m
+++ GyazzForiPhone/GYZGyazzAddViewController.m
@@ -103,8 +103,7 @@
     GYZGyazz *newGyazz = [[GYZGyazz alloc] initWithName:tcell.textField.text];
     __block GYZGyazz *__newGyazz = newGyazz;
     __block __weak typeof (self) __self = self;
-    NSString *url = [[newGyazz absoluteURLPath] stringByAppendingPathComponent:@"__access"];
-    [newGyazz accessToURL:url success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [newGyazz getPageListWithWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSString *jsonstr = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
         $(@"%@",jsonstr);
         if (!jsonstr || jsonstr.length < 10) {
